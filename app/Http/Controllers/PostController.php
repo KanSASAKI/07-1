95505905fb66d61df529b4151fb89afc530e6138
KanSASAKI <kaaahn.jp@outlook.jp>
2022-12-13 04:45:21 +0000

    カリキュラム08-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,20 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    /**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
     public function index(Post $post)
     {
         return view('posts/index')->with(['posts' => $post->get()]);  
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
 }
-
