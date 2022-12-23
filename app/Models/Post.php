b740f0d6e33cce04b9f1e057623d40c7f9163691
KanSASAKI <kaaahn.jp@outlook.jp>
2022-12-23 08:02:02 +0000

    カリキュラム09-2解説２まで完了。

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,7 +19,7 @@ class Post extends Model
     
     public function getPaginateByLimit(int $limit_count = 1)
     {
-        $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
     
     // Categoryに対するリレーション
